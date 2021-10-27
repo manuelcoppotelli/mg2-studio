54e75049638e292668cc7015b6657fb0e59356fc
Manuel Coppotelli <hi@manuelcoppotelli.me>
2021-10-28 01:23:04 +0200

    feat: recaptcha support

--- src/components/FormField.js
+++ src/components/FormField.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import _ from 'lodash';
+import { Recaptcha } from 'react-netlify-forms'
 
 export default function FormField(props) {
-
     let field = _.get(props, 'field', null);
     return (
         (_.get(field, 'input_type', null) === 'checkbox') ? (
         <div className="form-group form-checkbox">
-          <input type="checkbox" id={_.get(field, 'name', null)} name={_.get(field, 'name', null)}{...(_.get(field, 'is_required', null) ? ({required: true}) : null)}/>
+          <input type="checkbox" id={_.get(field, 'name', null)} name={_.get(field, 'name', null)}{...(_.get(field, 'is_required', null) ? ({required: true}) : null)} {...(_.get(props, 'changeHandle', null) ? ({onChange: _.get(props, 'changeHandle', null)}) : null)} />
           {_.get(field, 'label', null) && (
           <label htmlFor={_.get(field, 'name', null)}>{_.get(field, 'label', null)}</label>
           )}
@@ -18,7 +18,7 @@ export default function FormField(props) {
           <label htmlFor={_.get(field, 'name', null)}>{_.get(field, 'label', null)}</label>
           )}
           <div className="form-select-wrap">
-            <select id={_.get(field, 'name', null)} name={_.get(field, 'name', null)}{...(_.get(field, 'is_required', null) ? ({required: true}) : null)}>
+            <select id={_.get(field, 'name', null)} name={_.get(field, 'name', null)}{...(_.get(field, 'is_required', null) ? ({required: true}) : null)} {...(_.get(props, 'changeHandle', null) ? ({onChange: _.get(props, 'changeHandle', null)}) : null)} >
               {_.get(field, 'default_value', null) && (
               <option value="">{_.get(field, 'default_value', null)}</option>
               )}
@@ -29,13 +29,15 @@ export default function FormField(props) {
           </div>
         </div>
         ) : ((_.get(field, 'input_type', null) === 'recaptcha') ? (
-          <div data-netlify-recaptcha="true"></div>
+          <div className="form-group">
+            <Recaptcha siteKey={process.env.GATSBY_APP_SITE_RECAPTCHA_KEY} />
+          </div>
         ) : ((_.get(field, 'input_type', null) === 'textarea') ? (
         <div className="form-group">
           {_.get(field, 'label', null) && (
           <label htmlFor={_.get(field, 'name', null)}>{_.get(field, 'label', null)}</label>
           )}
-          <textarea name={_.get(field, 'name', null)} id={_.get(field, 'name', null)} rows="5"{...(_.get(field, 'default_value', null) ? ({placeholder: _.get(field, 'default_value', null)}) : null)}{...(_.get(field, 'is_required', null) ? ({required: true}) : null)}/>
+          <textarea name={_.get(field, 'name', null)} id={_.get(field, 'name', null)} rows="5"{...(_.get(field, 'default_value', null) ? ({placeholder: _.get(field, 'default_value', null)}) : null)}{...(_.get(field, 'is_required', null) ? ({required: true}) : null)} {...(_.get(props, 'changeHandle', null) ? ({onChange: _.get(props, 'changeHandle', null)}) : null)} />
           <span className="animate-border" aria-hidden="true" />
         </div>
         ) :
@@ -43,7 +45,7 @@ export default function FormField(props) {
           {_.get(field, 'label', null) && (
           <label htmlFor={_.get(field, 'name', null)}>{_.get(field, 'label', null)}</label>
           )}
-          <input type={_.get(field, 'input_type', null)} name={_.get(field, 'name', null)} id={_.get(field, 'name', null)}{...(_.get(field, 'default_value', null) ? ({placeholder: _.get(field, 'default_value', null)}) : null)}{...(_.get(field, 'is_required', null) ? ({required: true}) : null)} />
+          <input type={_.get(field, 'input_type', null)} name={_.get(field, 'name', null)} id={_.get(field, 'name', null)}{...(_.get(field, 'default_value', null) ? ({placeholder: _.get(field, 'default_value', null)}) : null)}{...(_.get(field, 'is_required', null) ? ({required: true}) : null)} {...(_.get(props, 'changeHandle', null) ? ({onChange: _.get(props, 'changeHandle', null)}) : null)} />
           <span className="animate-border" aria-hidden="true" />
         </div>
         )))
--- src/components/SectionDynamicForm.js
+++ src/components/SectionDynamicForm.js
@@ -1,44 +1,65 @@
 import React from 'react';
 import _ from 'lodash';
+import { NetlifyForm, Honeypot } from 'react-netlify-forms'
 
 import {htmlToReact, markdownify} from '../utils';
 import FormField from './FormField';
 
-export default class SectionDynamicForm extends React.Component {
-    render() {
-        let section = _.get(this.props, 'section', null);
-        return (
-            <section id={_.get(section, 'section_id', null)} className="block block-form outer">
-              <div className="inner">
-                {(_.get(section, 'title', null) || _.get(section, 'subtitle', null)) && (
-                <div className="block-header inner-sm">
-                  {_.get(section, 'title', null) && (
-                  <h2 className="block-title line-top">{_.get(section, 'title', null)}</h2>
-                  )}
-                  {_.get(section, 'subtitle', null) && (
-                  <p className="block-subtitle">{htmlToReact(_.get(section, 'subtitle', null))}</p>
-                  )}
-                </div>
-                )}
-                <div className="block-content inner-sm">
-                  {_.get(section, 'content', null) && (
-                  markdownify(_.get(section, 'content', null))
-                  )}
-                  <form name={_.get(section, 'form_id', null)} id={_.get(section, 'form_id', null)} {...(_.get(section, 'form_action', null) ? ({action: _.get(section, 'form_action', null)}) : null)}method="POST" data-netlify="true" data-netlify-recaptcha="true" >
-                    <div className="screen-reader-text">
-                      <label id={_.get(section, 'form_id', null) + '-bot-label'}>Don't fill this out if you're human: <input aria-labelledby={_.get(section, 'form_id', null) + '-bot-label'} name="bot-field" /></label>
-                    </div>
-                    <input type="hidden" aria-label={_.get(section, 'form_id', null) + '-name'} name="form-name" value={_.get(section, 'form_id', null)} />
+export default function SectionDynamicForm(props) {
+
+    let section = _.get(props, 'section', null);
+    return (
+        <section id={_.get(section, 'section_id', null)} className="block block-form outer">
+          <div className="inner">
+            {(_.get(section, 'title', null) || _.get(section, 'subtitle', null)) && (
+            <div className="block-header inner-sm">
+              {_.get(section, 'title', null) && (
+              <h2 className="block-title line-top">{_.get(section, 'title', null)}</h2>
+              )}
+              {_.get(section, 'subtitle', null) && (
+              <p className="block-subtitle">{htmlToReact(_.get(section, 'subtitle', null))}</p>
+              )}
+            </div>
+            )}
+            <div className="block-content inner-sm">
+              {_.get(section, 'content', null) && (
+              markdownify(_.get(section, 'content', null))
+              )}
+
+              <NetlifyForm
+                name={_.get(section, 'form_id', null)}
+                {...(_.get(section, 'form_action', null) ? ({action: _.get(section, 'form_action', null)}) : null)}
+                honeypotName='bot-field'
+                enableRecaptcha
+                onSuccess={(response, context) => {
+                  context.formRef.current.reset()
+                }}
+              >
+                {({ handleChange, success, error }) => (
+                  <>
+                    <Honeypot />
+                    {success && (
+                      <p>
+                        Thanks for contacting us!
+                      </p>
+                    )}
+                    {error && (
+                      <p>
+                        Sorry, we could not reach servers. Because it only works on Netlify,
+                        our GitHub demo does not provide a response.
+                      </p>
+                    )}
                     {_.map(_.get(section, 'form_fields', null), (field, field_idx) => (
-                      <FormField key={field_idx} {...this.props} field={field} />
+                      <FormField key={field_idx} {...props} field={field} changeHandle={handleChange} />
                     ))}
                     <div className="form-submit">
                       <button type="submit" className="button">{_.get(section, 'submit_label', null)}</button>
                     </div>
-                  </form>
-                </div>
-              </div>
-            </section>
-        );
-    }
+                  </>
+                )}
+              </NetlifyForm>
+            </div>
+          </div>
+        </section>
+    );
 }
